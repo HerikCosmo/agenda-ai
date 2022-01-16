62121b483b367478bd68e8558c81a71b538635b4
Herik Cosmo <herikcosmo.m@gmail.com>
2022-01-15 23:05:26 -0300

    build: add bootstrap v5.1 files

--- view/agenda/listar-contatos.php
+++ view/agenda/listar-contatos.php
@@ -5,8 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agenda</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
-    <h1>Agenda de Contatos</h1>
+    <div class="container">
+        <h1>Agenda de Contatos</h1>    
+    </div>
+    <script src="js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
