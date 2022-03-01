34351d840db673400b0d64e8e3d2e14efcebd267
Herik Cosmo <herikcosmo.m@gmail.com>
2022-03-01 10:49:19 -0300

    ajuste tabela contatos

--- view/agenda/index.php
+++ view/agenda/index.php
@@ -5,38 +5,35 @@
         <div class="row">
             <h3 class="text-start text-white py-3">Contatos</h3>
         </div>
-        <div class="row">
-            
-            <table class="table bg-light">
-                <thead>
-                    <tr>
-                        <th scope="col">Nome</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Telefone</th>
-                        <th></th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                <?php foreach($contatos as $contato): ?>
-                    <tr>
-                        <td><?php echo $contato->nome ?></td>
-                        <td><?php echo $contato->email ?></td>
-                        <td><?php echo $contato->telefone ?></td>
-                        <td><a class="btn btn-warning"href="/contato-edicao?id=<?php echo $contato->id ?>">Editar</a></td>
-                        <td><a class="btn btn-danger"href="/contato-excluir?id=<?php echo $contato->id ?>">Excluir</a></td>
-                    </tr>
-                <?php endforeach; ?>
-                </tbody>
-            </table>
+        <div class="row" style="overflow-x: auto;">
+            <div class="table-responsive">
+                <table class="table bg-light">
+                    <thead>
+                        <tr>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Telefone</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($contatos as $contato) : ?>
+                            <tr>
+                                <td><?php echo $contato->nome ?></td>
+                                <td style="word-break: break-all;"><?php echo $contato->email ?></td>
+                                <td><?php echo $contato->telefone ?></td>
+                                <td colspan="2">
+                                    <a class="btn btn-warning my-1" href="/contato-edicao?id=<?php echo $contato->id ?>">Editar</a>
+                                    <a class="btn btn-danger my-1" href="/contato-excluir?id=<?php echo $contato->id ?>">Excluir</a>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
-    <?php echo $_SESSION['mensagem'] ?? '' ?>
-    <!-- <?php foreach($contatos as $contato){ ?>
-        <?php echo $contato->nome ?> |
-        <a href="/contato-edicao?id=<?php echo $contato->id ?>">Editar</a> |
-        <a href="/contato-excluir?id=<?php echo $contato->id ?>">Excluir</a>
-        <br>        
-    <?php } ?>  -->
 </main>
+
 <?php require __DIR__ . '/../footer.php'; ?>
